298cdece40d2a7b7df9d2ee79033458e2d567f0f
worawich1011 <worawich1011@gmail.com>
2022-03-22 15:51:48 +0700

    finish code

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open',() => console.log('Connected to Database'))
 
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
+
 app.get("/", (req,res)=>{
     res.send("Hello Node.js REST Server")
 })
